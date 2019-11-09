ebbc8bd13edeb9b8f76d3e71f0c556c136512428
John Dupuy <jdupuy98@gmail.com>
2019-11-09 12:18:50 -0600

    Fixed URL ref in readme

--- src/bson.nim
+++ src/bson.nim
@@ -127,7 +127,7 @@
 ## ======
 ##
 ## Large portions of this code were pulled from the nimongo project, a scalable
-## pure-nim mongodb driver. See `https://github.com/SSPkrolik/nimongo`__
+## pure-nim mongodb driver. See https://github.com/SSPkrolik/nimongo
 ##
 ## However, this library is NOT compatilible with nimongo, as nimongo relies on an
 ## internal implementation of BSON.
